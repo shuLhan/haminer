7e7c491d186280ddaee508bd46ade6fa21058c78
Shulhan <ms@kilabit.info>
2022-08-19 00:04:13 +0700

    all: relicensing the haminer to GPL v3
    See https://kilabit.info/journal/2022/gpl/ for more information.

--- forwarder.go
+++ forwarder.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2019 M. Shulhan <ms@kilabit.info>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package haminer
 
 // Forwarder define an interface to forward parsed HAProxy log to storage
--- questdb_client.go
+++ questdb_client.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2022 M. Shulhan <ms@kilabit.info>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package haminer
 
 import (
